7bbabc9d73e33198533b4fb1a5f520a7efa77425
Ariel Ayala <arielayalamail@gmail.com>
2021-12-14 20:20:42 -0300

    Create crowdfunding.sol

---
+++ crowdfunding.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Crowfunding {
+
+     bool isFundable;
+     uint256 goalAmount; 
+     uint256 totalFunded;
+     address owner;
+     uint256 requiredFunds;
+     
+     constructor(){
+         goalAmount = 0;
+         owner = msg.sender;
+         totalFunded = 0;
+         isFundable = true;
+     }
+
+     // El modifier permite cambiar el compoprtamiento de funciones, en este caso solo queria asegurarme que solo el creador del contrato pudiera mover el goalAmount
+     modifier onlyOwner{
+         require(msg.sender == owner, "You need to be the owner from this contract to change the goalAmount");
+         _;
+     }
+
+     //Aqui ponemos la meta a recaudar,solamente el que iniciaiza el contrato puede cambiar este valor
+     function setGoalAmount(uint256 amount) public onlyOwner {
+         goalAmount = amount;
+     }
+     
+     function viewGoalAmount() public view returns(uint256) {
+         return goalAmount;
+     }
+     
+     function changeProjectState(bool state) public onlyOwner{
+         isFundable = state;
+     }
+
+     //Aqui inicia la funcion para fondear el proyecto
+     function fundproject() public payable {
+         //Primero evaluamos si el owner del contrato mantiene abiertas las donaciones
+         require(isFundable, "Owner has decided to stop this fundraising for a while. Stay tuned");
+
+         //Comprobamos que el total que se ha fondeado sea menor a la meta
+         require(totalFunded < goalAmount, "goalAmount already achieved so you are  not able to fund this anymore");
+
+         // La persona mande un minimo, mayor que cero
+         require(msg.value != uint(0), "Please add some funds to contribuite to the project");
+
+         //Comprobamos que el valor que quiere fondear no exceda con a meta que tenemos
+         require(totalFunded + msg.value <= goalAmount,"unable to add more funds, check amount remaining for our goalAmount");
+
+         //Se actualiza el total fondeado al contrato
+         totalFunded += msg.value;
+     }
+     //Esta funcion nos sirve para que la persona pueda ver cuanto se necesita para alcanzar la meta, asi no tendra que estar adivinando cuanto depositar maximo
+     function viewRemaining() public view returns(uint256){
+         uint256 remainingFunds = goalAmount - totalFunded;
+         return remainingFunds;
+     }
+     
+ }
