f6f97526528f0b8fa323bcbbef6dfd763f52d166
Ariel Ayala <arielayalamail@gmail.com>
2021-12-15 20:21:39 -0300

    Validacion - Autor no puede fondear el proyecto
    Se valida que el author no puede agregar fondos al proyeto

--- crowdfunding.sol
+++ crowdfunding.sol
@@ -22,6 +22,12 @@ contract Crowfunding {
          require(msg.sender == owner, "You need to be the owner from this contract to change the goalAmount");
          _;
      }
+     
+     // Se valida que el author no puede agregar fondos al proyeto
+     modifier onlyOtherFunders{
+         require(msg.sender != owner, "The author of the contract can't fund the project");
+         _;
+     }
 
      //Aqui ponemos la meta a recaudar,solamente el que iniciaiza el contrato puede cambiar este valor
      function setGoalAmount(uint256 amount) public onlyOwner {
@@ -32,12 +38,12 @@ contract Crowfunding {
          return goalAmount;
      }
      
-     function changeProjectState(bool state) public onlyOwner{
+     function changeProjectState(bool state) public onlyOwner {
          isFundable = state;
      }
 
      //Aqui inicia la funcion para fondear el proyecto
-     function fundproject() public payable {
+     function fundproject() public payable onlyOtherFunders {
          //Primero evaluamos si el owner del contrato mantiene abiertas las donaciones
          require(isFundable, "Owner has decided to stop this fundraising for a while. Stay tuned");
 
